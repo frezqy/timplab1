41476bbc6884809f47ced60d4b00d5b7442b2c6d
frezqy <lox_2000_86@email.com>
2026-05-04 18:29:52 +0700

    fix

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { createItem } from '../api';
-import './Form.css';
 
 function Form() {
   const navigate = useNavigate();
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getItems, deleteItem } from '../api';
-import './Home.css';
 
 function Home() {
   const [incidents, setIncidents] = useState([]);
